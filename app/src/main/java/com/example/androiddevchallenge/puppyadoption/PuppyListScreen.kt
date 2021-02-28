213bcbc096ccebf408455aca5480ad797d684c5a
Palash <mobiledev.palash@gmail.com>
2021-02-28 19:03:20 +0530

    Resolved ktlint

--- app/src/main/java/com/example/androiddevchallenge/puppyadoption/PuppyListScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/puppyadoption/PuppyListScreen.kt
@@ -8,7 +8,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.LocalContentAlpha
+import androidx.compose.material.Divider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
